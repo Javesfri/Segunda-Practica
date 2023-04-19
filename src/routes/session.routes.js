52b8fc769a3c589b26f4b3642fbdb484c9705f80
Javier Frick <javesfri@gmail.com>
2023-04-19 00:02:09 -0300

    JWT ERROR RESOLVED

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { destroySession, isLogin, testLogin} from "../controllers/session.controller.js";
+import { passportError, roleVerification } from "../utils/errorMessages.js";
 import passport from "passport";
+
 const routerSession = Router()
 
 routerSession.post("/login", passport.authenticate('login'), testLogin)
 routerSession.get("/login",isLogin,(req,res)=>{
     if(!req.session.user)
         res.render("login", {})
+})
+routerSession.get("/current", passportError('jwt'), roleVerification('User'), (req, res) => {
+    res.send(req.user)
 })
 routerSession.get("/logout", destroySession)
 
